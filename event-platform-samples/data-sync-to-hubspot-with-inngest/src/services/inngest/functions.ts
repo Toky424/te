6e4885d2f517eeef890cdabcfe6deb6bbfbc9ca0
Carlos Mostek <carlos.mostek@okta.com>
2025-02-11 12:05:09 -0600

    Fix undefined response based on code review

--- event-platform-samples/data-sync-to-hubspot-with-inngest/src/services/inngest/functions.ts
+++ event-platform-samples/data-sync-to-hubspot-with-inngest/src/services/inngest/functions.ts
@@ -8,65 +8,70 @@ export const handleAuth0Event = client.createFunction(
     retries: 10, // this will retry for hours, can set to much longer if we need to retry for weeks, though there's likely a better way to handle this...
   },
   async ({ event, step, hubspot }) => {
-    let response = {};
+    const response = await step.run(
+      `process auth0/${event.data.payload.type}`,
+      async () => {
+        console.log("Received Event: ", event.data.payload);
 
-    await step.run(`process auth0/${event.data.payload.type}`, async () => {
-      console.log("Received Event: ", event.data.payload);
+        try {
+          const userEvent: UserEvent = event.data.payload;
 
-      try {
-        const userEvent: UserEvent = event.data.payload;
+          const user = await hubspot.getUser(userEvent);
 
-        const user = await hubspot.getUser(userEvent);
-
-        switch (userEvent.type) {
-          case "user.created":
-          case "user.initialized":
-            if (user) {
-              console.log("skipping user because it already exists: ", user);
-              diffUser(user, userEvent);
+          switch (userEvent.type) {
+            case "user.created":
+            case "user.initialized":
+              if (user) {
+                console.log("skipping user because it already exists: ", user);
+                diffUser(user, userEvent);
+                break;
+              }
+            case "user.updated":
+              if (!user) {
+                await hubspot.createUser(userEvent);
+                break;
+              }
+              const userTimestamp = parseInt(
+                user.auth0_last_event_timestamp || "0"
+              );
+              const eventTimestamp = new Date(userEvent.time).getTime();
+              // Check event time vs last event processed on the user
+              if (eventTimestamp > userTimestamp) {
+                await hubspot.updateUser(user, userEvent);
+                break;
+              }
+              console.log(
+                `Not updating user because ${eventTimestamp} <= ${userTimestamp}`
+              );
               break;
-            }
-          case "user.updated":
-            if (!user) {
-              await hubspot.createUser(userEvent);
+            case "user.deleted":
+              if (!user) {
+                console.log("Can't delete non-existing user");
+                break;
+              }
+
+              await hubspot.deleteUser(user);
               break;
-            }
-            const userTimestamp = parseInt(
-              user.auth0_last_event_timestamp || "0"
+          }
+
+          return {
+            message: "User event processed successfully",
+          };
+        } catch (e: unknown) {
+          if (e instanceof Error) {
+            console.error("Failed to process event", e, event);
+            console.error(e.message);
+          } else {
+            console.error(
+              "Failed to process event for unknown reason",
+              e,
+              event
             );
-            const eventTimestamp = new Date(userEvent.time).getTime();
-            // Check event time vs last event processed on the user
-            if (eventTimestamp > userTimestamp) {
-              await hubspot.updateUser(user, userEvent);
-              break;
-            }
-            console.log(
-              `Not updating user because ${eventTimestamp} <= ${userTimestamp}`
-            );
-            break;
-          case "user.deleted":
-            if (!user) {
-              console.log("Can't delete non-existing user");
-              break;
-            }
-
-            await hubspot.deleteUser(user);
-            break;
+          }
+          throw e;
         }
-
-        return (response = {
-          message: "User event processed successfully",
-        });
-      } catch (e: unknown) {
-        if (e instanceof Error) {
-          console.error("Failed to process event", e, event);
-          console.error(e.message);
-        } else {
-          console.error("Failed to process event for unknown reason", e, event);
-        }
-        throw e;
       }
-    });
+    );
     return response;
   }
 );
